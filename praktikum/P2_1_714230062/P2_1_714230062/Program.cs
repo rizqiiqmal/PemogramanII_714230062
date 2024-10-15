88228cf975bac83c00fe1a5db00ab9dc9e97c024
rizqiiqmal <rizqiiqmal805@gmail.com>
2024-10-15 16:13:49 +0700

    tugas pertama

---
+++ praktikum/P2_1_714230062/P2_1_714230062/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714230062
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Masukan angka pertama: ");
+            double angkaPertama =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Masukan angka kedua: ");
+            double angkaKedua = Convert.ToDouble(Console.ReadLine());
+
+            double penjumlahan = angkaPertama + angkaKedua;
+            double pengurangan = angkaPertama - angkaKedua;
+            double perkalian = angkaPertama * angkaKedua;
+            double pembagian = angkaPertama / angkaKedua;
+
+            Console.WriteLine($"{angkaPertama} + {angkaKedua} = {penjumlahan}");
+            Console.WriteLine($"{angkaPertama} - {angkaKedua} = {pengurangan}");
+            Console.WriteLine($"{angkaPertama} * {angkaKedua} = {perkalian}");
+            Console.WriteLine($"{angkaPertama} / {angkaKedua} = {pembagian}");
+
+        }
+    }
+}
